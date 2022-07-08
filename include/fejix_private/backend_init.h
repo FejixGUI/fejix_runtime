2401c0fbfa222ecac1566ca226761571c96e4ea6
MarkLagodych <lgd.mrk@gmail.com>
2022-07-08 16:42:01 +0300

    Renamed backend_generic.h -> backend_init.h

---
+++ include/fejix_private/backend_init.h
@@ -0,0 +1,15 @@
+#ifndef _FEJIX_PRIVATE_BACKEND_INIT_H_
+#define _FEJIX_PRIVATE_BACKEND_INIT_H_
+
+#include <fejix_runtime/definitions.h>
+#include <fejix_runtime/internal/backend.h>
+
+struct FjBackendParams {
+    struct FjApp *app;
+    uint32_t backend0;
+    uint32_t backend1;
+};
+
+uint32_t fjBackendInitApp(struct FjApp *app, struct FjBackend *backend, struct FjBackendParams *params);
+
+#endif // _FEJIX_PRIVATE_BACKEND_INIT_H_
